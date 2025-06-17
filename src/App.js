9b1a7be9abbc73bb23ac876e6dfa3c669f57401e
CH. HITESH <rajasampathhitesh@gmail.com>
2025-06-17 10:52:21 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,60 @@
+import { useState } from 'react';
+import './calculator.css';
+import Keypad from './keypad.js';
+function App(){
+  let [input,setInput]=useState("");
+  
+function handleClick(value) {
+  const operators = ['+', '-', '*', '/', '%'];
+  const lastChar = input[input.length - 1];
 
+  if (operators.includes(value)) {
+    if (input === '') return; 
+
+    if (operators.includes(lastChar)) {
+      setInput(input.slice(0, -1) + value);
+      return;
+    }
+  }
+
+  if (value === '.') {
+    const parts = input.split(/[+\-*%]/);
+    const currentPart = parts[parts.length - 1];
+    if (currentPart.includes('.')) {
+      return;
+    }
+  } 
+  
+  setInput(input + value);
+}
+
+
+  function handleClear(){
+    setInput("");
+  }
+
+  function resultCal(){
+    setInput(eval(input));
+  }
+
+  function backspace(){
+    setInput(input.slice(0, -1));
+  }
+
+  function handleSquare(){
+    setInput(input*input);
+  }
+
+  return(
+    <div className="container">
+        <h1>&nbsp;&nbsp;Calculator Application</h1>
+        <br></br>
+        <div className="box">
+            <input type="text" className='output' value={input} readOnly/>
+            <Keypad handleClick={handleClick} handleClear={handleClear} resultCal={resultCal} backspace={backspace} handleSquare={handleSquare}/>
+        </div>
+    </div>
+  )
+}
+
+export default App;
